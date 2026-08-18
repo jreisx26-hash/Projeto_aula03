611afa70232e1ae77bdb7b282333ea0f6ed065e1
JOÃO PEDRO DOS REIS PEREIRA <26171613@edu119.local>
2026-08-18 09:50:32 -0300

    subindo para o git

---
+++ 01-funçôes/05-funcao.condicional.js
@@ -0,0 +1,10 @@
+function verificaNota(media){
+    // if(media >= 7){
+    //     return "aprovado"
+    // }
+    // return "reprovado"
+
+    return media >= 7 ? "aprovado " : "reprovado";
+}
+
+console.log(verificaNota(8));
