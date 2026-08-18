9775301e6858efba0921b36ec364772646c28dac
JOÃO PEDRO DOS REIS PEREIRA <26171613@edu119.local>
2026-08-18 14:15:14 -0300

    subindo

---
+++ 02-métodos-matriz/04-find.js
@@ -0,0 +1,9 @@
+const alunos = ["Giovanni", "Guatavo","Ingrid", "Isabela"];
+
+const aluno = alunos.find((nome) => nome === "Ingrid");
+
+console.log(aluno);
+
+const aluno1= aluno.find((nome) => nome === "Isabela");
+
+console.log(aluno1);
---
+++ 02-métodos-matriz/05-find-index.js
@@ -0,0 +1,9 @@
+let nomes = ["João", "Júlia", "Larissa","Lívia"];
+
+const indice = nomes.findIndex((n) => n === "Larissa");
+
+console.log(indice);
+
+const indice1 = nomes.findIndex((n) => n === "andré");
+
+console.log(indice1);
