ba6dbf69f028297cae1d0721ff4b893901a957a9
JOÃO PEDRO DOS REIS PEREIRA <26171613@edu119.local>
2026-08-18 14:45:13 -0300

    subindo

---
+++ 02-métodos-matriz/07-every.js
@@ -0,0 +1,5 @@
+const notas = [7, 4,9,10];
+
+const todosAprovados = notas.every((nota) => nota >= 7);
+
+console.log(todosAprovados);
---
+++ 02-métodos-matriz/reduce.js
@@ -0,0 +1,14 @@
+const valores = [10 , 20, 30, 40, 50];
+
+const total = valores.reduce(
+    (acumulador, valor) => acumulador + valor , 0
+);
+
+console.log(total);
+
+//0 + 10 = 10
+//10 + 20 = 30
+//30 + 30 = 60
+//60 + 40 = 100
+//100 + 50 = 150
+
